ec11bee667ecba600a74829ff7e83e57971d5fef
atul-dev-ai <paulatul020@gmail.com>
2026-07-24 21:06:23 +0600

    not operator

---
+++ phase_02_variables_data_types/logical_operators/not.java
@@ -0,0 +1,11 @@
+package phase_02_variables_data_types.logical_operators;
+
+public class not {
+    public static void main(String[] args) {
+        boolean result = true;
+        boolean notResult = !result;
+
+        System.out.println("The result is: " + result);
+        System.out.println("The negation of the result is: " + notResult);
+    }
+}
