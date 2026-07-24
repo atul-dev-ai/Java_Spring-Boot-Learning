48501f1f9f77c1c5bc702dfcd05987207682d1ac
atul-dev-ai <paulatul020@gmail.com>
2026-07-24 16:10:25 +0600

    modulus

---
+++ phase_02_variables_data_types/operators/modulus.java
@@ -0,0 +1,11 @@
+package phase_02_variables_data_types.operators;
+
+public class modulus {
+    public static void main(String[] args) {
+        int a = 10;
+        
+        System.out.println(a % 3);
+        /* ekhane output 1 ashbe. 
+        percentage use kore vagsesh ber kora hoy. */
+    }
+}
