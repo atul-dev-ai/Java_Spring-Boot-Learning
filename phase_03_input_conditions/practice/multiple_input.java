ae78ed5cd44f46c813bcc2505ecbf8c1b2c572b0
atul-dev-ai <paulatul020@gmail.com>
2026-07-24 22:50:51 +0600

    multiple input

--- phase_03_input_conditions/practice/multiple_input.java
+++ phase_03_input_conditions/practice/multiple_input.java
@@ -1,5 +1,18 @@
 package phase_03_input_conditions.practice;
 
+import java.util.Scanner;
 public class multiple_input {
-    
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter your name: ");
+        String name = input.nextLine();
+
+        System.out.print("Enter you age: ");
+        int age = input.nextInt();
+
+        System.out.print("Enter your height: ");
+        double height = input.nextDouble();
+
+        System.out.println("Hello, " + name + "! your age is: " + age + " and your height is: " + height);
+    }
 }
