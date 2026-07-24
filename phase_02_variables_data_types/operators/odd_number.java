e036e364d4a6e1e07792b1b0380162649bd526f6
atul-dev-ai <paulatul020@gmail.com>
2026-07-24 20:46:17 +0600

    odd number practice

--- phase_02_variables_data_types/operators/odd_number.java
+++ phase_02_variables_data_types/operators/odd_number.java
@@ -1,5 +1,24 @@
 package phase_02_variables_data_types.operators;
 
 public class odd_number {
-    
+    // int number = 11;
+    // public static void main(String[] args) {
+    //     odd_number obj = new odd_number();
+    //     if (obj.number % 2 != 0) {
+    //         System.out.println(obj.number + " is an odd number.");
+    //     }
+    //     else {
+    //         System.out.println(obj.number + " is not an odd number.");
+    //     }
+    //     System.out.println(number);
+    // }
+    public static void main(String[] args) {
+        int number = 11;
+        if (number % 2 != 0) {
+            System.out.println(number + " is an odd number");
+        }
+        else {
+            System.out.println(number + " is not an odd number");
+        }
+    }
 }
