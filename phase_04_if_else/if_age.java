0789383492f2a1e3bd3ae70ed8af5a890f3056a2
atul-dev-ai <paulatul020@gmail.com>
2026-07-24 23:07:07 +0600

    if

---
+++ phase_04_if_else/if_age.java
@@ -0,0 +1,15 @@
+package phase_04_if_else;
+import java.util.Scanner;
+public class if_age {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter your age: ");
+        int age = input.nextInt();
+        if (age >= 18) {
+            System.out.println("You are eligibel for drinking");
+        }
+        else {
+            System.out.println("You are not eligibel for drinking");
+        }
+    }
+}
