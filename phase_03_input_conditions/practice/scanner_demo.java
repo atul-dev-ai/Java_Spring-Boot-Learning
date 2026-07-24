1fc0df97c60d3308a4f093ef0421a2d0342cdc67
atul-dev-ai <paulatul020@gmail.com>
2026-07-24 22:06:14 +0600

    note added

--- phase_03_input_conditions/practice/scanner_demo.java
+++ phase_03_input_conditions/practice/scanner_demo.java
@@ -4,7 +4,8 @@ import java.util.Scanner;
 
 public class scanner_demo {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
+        Scanner input = new Scanner(System.in); // ekhane Scanner class name, ar input holo object name. new mane memory allocate kora(notun object create kora) ar Scanner() holo constructor(object toiri kore). 
+        // System.in mane holo keyboard theke input nite hobe. 
         System.out.print("Enter you name: ");
         String name = input.nextLine();
         System.out.print("Enter your age: ");
