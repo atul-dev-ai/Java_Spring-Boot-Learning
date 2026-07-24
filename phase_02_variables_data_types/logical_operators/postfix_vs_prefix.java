829e414606857a1b3038f3e7d34a04a393b8beb8
atul-dev-ai <paulatul020@gmail.com>
2026-07-24 21:20:48 +0600

    postfix vs prefix

---
+++ phase_02_variables_data_types/logical_operators/postfix_vs_prefix.java
@@ -0,0 +1,18 @@
+package phase_02_variables_data_types.logical_operators;
+
+public class postfix_vs_prefix {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 10;
+
+        System.out.println(++a);
+        System.out.println(b++);
+
+        String firstName = "Atul";
+        String lastName = "Kumar";
+        System.out.println(firstName + " " + lastName);
+
+        int age = 21;
+        System.out.println("My age is: " + age);
+    }
+}
