4ef98448a66b4062893621a96226f912907ceeee
atul-dev-ai <paulatul020@gmail.com>
2026-07-24 12:29:29 +0600

    variables practice

---
+++ phase_02_variables_data_types/practice/variables.java
@@ -0,0 +1,19 @@
+package phase_02_variables_data_types.practice;
+
+public class variables {
+    public static void main(String[] args) {
+        String name = "Atul Paul";
+        int age = 21;
+        double cgpa = 3.80;
+        char grade = 'A';
+        boolean isStudent = true;
+        char currency = '$';
+
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(cgpa);
+        System.out.println(grade);
+        System.out.println(isStudent);
+        System.out.println(currency);
+    }
+}
