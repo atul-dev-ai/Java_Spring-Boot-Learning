ffd2322be5be06ca65aa91d44bd5acb393e0982a
atul-dev-ai <paulatul020@gmail.com>
2026-07-24 22:11:05 +0600

    integer input

--- phase_03_input_conditions/practice/integer_input.java
+++ phase_03_input_conditions/practice/integer_input.java
@@ -1,5 +1,15 @@
 package phase_03_input_conditions.practice;
 
+import java.util.Scanner;
+
 public class integer_input {
-    
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter your age: ");
+
+        int age = input.nextInt(); // nextInt() method takes integer input from the user.
+        System.out.println("You are " + age + " years old.");
+        input.close(); // Closing the scanner to prevent resource leaks.
+    }
 }
