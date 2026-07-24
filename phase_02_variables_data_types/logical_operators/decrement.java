e368ac687900996149c0fb20b9f2dd70d66c1bc6
atul-dev-ai <paulatul020@gmail.com>
2026-07-24 21:12:45 +0600

    decrementing

--- phase_02_variables_data_types/logical_operators/decrement.java
+++ phase_02_variables_data_types/logical_operators/decrement.java
@@ -1,10 +1,11 @@
 package phase_02_variables_data_types.logical_operators;
 
-public class incrment {
+public class decrement {
     public static void main(String[] args) {
-        int x = 5;
+        int x = 10;
         System.out.println("Initial value of x: " + x);
-        x++; // Incrementing x by 1
-        System.out.println("Value of x after increment: " + x);
+        x--; // Decrementing x by 1 .
+
+        System.out.println("Value of x after decrement: " + x);
     }
 }
---
+++ phase_02_variables_data_types/logical_operators/increment.java
@@ -0,0 +1,10 @@
+package phase_02_variables_data_types.logical_operators;
+
+public class increment {
+    public static void main(String[] args) {
+        int x = 10;
+        System.out.println("Initial value of x: " + x);
+        x++; // Incrementing x by 1
+        System.out.println("Value of x after increment: " + x);
+    }
+}
