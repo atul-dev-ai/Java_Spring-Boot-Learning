1a0fef09c5dd73df8fe5445d9a436d7aaf9c4929
atul-dev-ai <paulatul020@gmail.com>
2026-07-24 11:42:08 +0600

    bio practice

--- phase_01_Foundation/practice/prac.java
+++ phase_01_Foundation/practice/prac.java
@@ -1,11 +1,16 @@
 public class prac {
+    // public static void main(String[] args) {
+    //     System.out.println("Name: Atul Paul");
+    //     System.out.println("University: Daffodil International University");
+    //     System.out.println("Department: Computing & Information System");
+    //     System.out.println("I will become a Java Developer");
+        
+    // }
+
     public static void main(String[] args) {
         System.out.println("Name: Atul Paul");
         System.out.println("University: Daffodil International University");
         System.out.println("Department: Computing & Information System");
         System.out.println("I will become a Java Developer");
-        
     }
-
-
 }
