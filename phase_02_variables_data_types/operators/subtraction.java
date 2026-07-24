3e9b2c46c3df9d96085ebac2e95520d185cd7142
atul-dev-ai <paulatul020@gmail.com>
2026-07-24 12:37:41 +0600

    subtraction practice

---
+++ phase_02_variables_data_types/operators/subtraction.java
@@ -0,0 +1,12 @@
+package phase_02_variables_data_types.operators;
+
+public class subtraction {
+    public static void main(String[] args) {
+        int a = 30;
+        int b = 30;
+        int c = 40;
+        int d = 10;
+
+        System.out.println(a - b + c - d);
+    }
+}
