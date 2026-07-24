00ac104c1091342bcddc7e4070eb1ea4e55ff20e
atul-dev-ai <paulatul020@gmail.com>
2026-07-24 21:09:51 +0600

    increment operator

--- phase_02_variables_data_types/logical_operators/incrment.java
+++ phase_02_variables_data_types/logical_operators/incrment.java
@@ -1,5 +1,10 @@
 package phase_02_variables_data_types.logical_operators;
 
 public class incrment {
-    
+    public static void main(String[] args) {
+        int x = 5;
+        System.out.println("Initial value of x: " + x);
+        x++; // Incrementing x by 1
+        System.out.println("Value of x after increment: " + x);
+    }
 }
