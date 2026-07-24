e49283c45b92a8bff52612776cff6b3d910b3bcf
atul-dev-ai <paulatul020@gmail.com>
2026-07-24 16:04:17 +0600

    addition math practice

---
+++ phase_02_variables_data_types/operators/additioino.java
@@ -0,0 +1,12 @@
+package phase_02_variables_data_types.operators;
+
+public class additioino {
+    public static void main(String[] args) {
+        int a = 20;
+        int b = 30;
+
+        int result = a + b;
+
+        System.out.println(result);
+    }
+}
