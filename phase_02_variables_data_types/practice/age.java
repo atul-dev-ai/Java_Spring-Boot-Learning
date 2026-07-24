15ce8c076d9ef89ec2ed88056ca28ec937c9c9a1
atul-dev-ai <paulatul020@gmail.com>
2026-07-24 12:20:19 +0600

    integer datatype practice 1

---
+++ phase_02_variables_data_types/practice/age.java
@@ -0,0 +1,8 @@
+package phase_02_variables_data_types.practice;
+
+public class age {
+    public static void main(String[] args) {
+        int age = 21;
+        System.out.println("My age is "+ age);
+    }
+}
