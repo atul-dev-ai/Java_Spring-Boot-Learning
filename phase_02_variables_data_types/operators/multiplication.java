cd0279cf385c2d25135b418379585bd3b8c5341f
atul-dev-ai <paulatul020@gmail.com>
2026-07-24 16:05:21 +0600

    multiplication.java

---
+++ phase_02_variables_data_types/operators/multiplication.java
@@ -0,0 +1,10 @@
+package phase_02_variables_data_types.operators;
+
+public class multiplication {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 6;
+
+        System.out.println(a * b);
+    }
+}
