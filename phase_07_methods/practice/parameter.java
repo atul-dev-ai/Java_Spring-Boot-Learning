af5984c4b970679a523dadba230a1af2abb41c17
atul-dev-ai <paulatul020@gmail.com>
2026-07-25 19:58:01 +0600

    parameter

--- phase_07_methods/practice/parameter.java
+++ phase_07_methods/practice/parameter.java
@@ -1,12 +1,12 @@
 package phase_07_methods.practice;
 
 public class parameter {
-    static void hello(String name) {
+    static void hello(String name) { // ekhane name hocche Parameter. 
         System.out.println("Hello " + name);
     }
 
     public static void main(String[] args) {
-        hello("Atul");
+        hello("Atul"); // ekhaen Atul hocche arguments
         hello("Ankit");
     }
 }
