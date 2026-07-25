3f01e165bfcce20455032d0a10e032b57c8da0c1
atul-dev-ai <paulatul020@gmail.com>
2026-07-25 06:58:53 +0600

    negatice positive

--- phase_04_if_else/positive_negative.java
+++ phase_04_if_else/positive_negative.java
@@ -1,5 +1,19 @@
 package phase_04_if_else;
 
+import java.util.Scanner;
+
 public class positive_negative {
-    
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int number = input.nextInt();
+
+        if (number > 0) {
+            System.out.println("This number is positive number.");
+        }
+        else {
+            System.out.println("This number is negative.");
+        }
+    }
 }
