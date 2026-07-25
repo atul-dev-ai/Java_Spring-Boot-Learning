dfed0e903d3d492087390b48c40a9710a3ac14e5
atul-dev-ai <paulatul020@gmail.com>
2026-07-25 13:14:07 +0600

    reversing

---
+++ phase_05_loops/practice/reverse.java
@@ -0,0 +1,9 @@
+package phase_05_loops.practice;
+
+public class reverse {
+    public static void main(String[] args) {
+        for (int i = 10; i >= 1; i--) {
+            System.out.println(i);
+        }
+    }
+}
