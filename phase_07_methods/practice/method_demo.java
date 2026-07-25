78d85596a239afd4439cf7edd93a0f69c3bcc8f1
atul-dev-ai <paulatul020@gmail.com>
2026-07-25 19:52:36 +0600

    nethod demo

---
+++ phase_07_methods/practice/method_demo.java
@@ -0,0 +1,19 @@
+package phase_07_methods.practice;
+
+public class method_demo {
+
+    static void hello() {
+
+        System.out.println("Hello Java");
+
+    }
+
+    public static void main(String[] args) {
+
+        hello();
+        hello();
+        hello();
+
+    }
+
+}
