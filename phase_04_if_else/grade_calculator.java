d59ddd1db1faadba12aa6935333b09a52a739a03
atul-dev-ai <paulatul020@gmail.com>
2026-07-25 07:20:08 +0600

    grade calculation with if else, else if

---
+++ phase_04_if_else/grade_calculator.java
@@ -0,0 +1,37 @@
+package phase_04_if_else;
+
+import java.util.Scanner;
+
+public class grade_calculator {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter your marks: ");
+        int marks = input.nextInt();
+
+        if (marks >= 90 && marks <=100) {
+            System.out.println("Grade A+");
+        }
+        else if (marks >= 80 && marks < 90) {
+            System.out.println("Grade A");
+        }
+        else if (marks >= 70 && marks < 80) {
+            System.out.println("Grade A-");
+        }
+        else if (marks >= 60 && marks < 70) {
+            System.out.println("Grade B");
+        }
+        else if (marks >= 50 && marks < 60) {
+            System.out.println("Grade B-");
+        }
+        else if (marks >= 33 && marks < 50) {
+            System.out.println("Grade C");
+        }
+        else if (marks < 33) {
+            System.out.println("Fail");
+        }
+        else {
+            System.out.println("Invalid marks");
+        }
+    }
+}
---
+++ phase_04_if_else/nested_if.java
@@ -0,0 +1,5 @@
+package phase_04_if_else;
+
+public class nested_if {
+    
+}
