d1b26d1ec27acfbaf994bc19cfbcfdfbe18462b2
atul-dev-ai <paulatul020@gmail.com>
2026-07-25 07:22:38 +0600

    nested if else practice

--- phase_04_if_else/nested_if.java
+++ phase_04_if_else/nested_if.java
@@ -1,5 +1,21 @@
 package phase_04_if_else;
 
+
 public class nested_if {
-    
+    public static void main(String[] args) {
+        int number = 20;
+
+        if (number > 0) {
+            System.out.println("This number is positive number.");
+            if (number % 2 == 0) {
+                System.out.println("This is an even number.");
+            }
+            else {
+                System.out.println("This is an odd number.");
+            }
+        }
+        else {
+            System.out.println("This number is negative.");
+        }
+    }
 }
