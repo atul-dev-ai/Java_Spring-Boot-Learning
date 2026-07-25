4ebcd502f38b54849a867252db0b1341c05aa9e5
atul-dev-ai <paulatul020@gmail.com>
2026-07-25 19:56:44 +0600

    with parameter

---
+++ phase_07_methods/practice/parameter.java
@@ -0,0 +1,12 @@
+package phase_07_methods.practice;
+
+public class parameter {
+    static void hello(String name) {
+        System.out.println("Hello " + name);
+    }
+
+    public static void main(String[] args) {
+        hello("Atul");
+        hello("Ankit");
+    }
+}
