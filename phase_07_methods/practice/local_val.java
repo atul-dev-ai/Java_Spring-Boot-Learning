11e104fcb3d1335aec675cd4a4bb7a7e34b14afa
atul-dev-ai <paulatul020@gmail.com>
2026-07-26 21:39:57 +0600

    local variable

---
+++ phase_07_methods/practice/local_val.java
@@ -0,0 +1,11 @@
+package phase_07_methods.practice;
+
+public class local_val {
+    static void locals() {
+        int a = 100; // ekhane locals ta local variable.
+    }
+    public static void main(String[] args) {
+        locals();
+        // System.out.println(a); // ekhane a er value print kora jabe na karon a holo local variable.
+    }
+}
