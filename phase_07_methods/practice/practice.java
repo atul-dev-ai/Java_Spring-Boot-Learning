2da64eba5b2629342e9f69b5e8f80c2448f5e4e8
atul-dev-ai <paulatul020@gmail.com>
2026-07-26 21:44:25 +0600

    practice

---
+++ phase_07_methods/practice/practice.java
@@ -0,0 +1,19 @@
+package phase_07_methods.practice;
+
+public class practice {
+    static int add(int a, int b) {
+        return a + b;
+    }
+
+    static int multiply (int a, int b) {
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+        int sum = add(5, 20);
+        System.out.println(sum);
+
+        int product = multiply(20, 100);
+        System.out.println(product);
+    }
+}
